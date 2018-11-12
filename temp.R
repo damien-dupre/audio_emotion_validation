e744ca13153b312cbecc706930959ea9c884774b
damien-dupre <damien.dupre.ac@gmail.com>
2018-11-12 16:19:24 +0000

    abstract

--- temp.R
+++ temp.R
@@ -21,7 +21,6 @@ test <- data.table::fread(here::here("./data","audeering_gemep.csv")) %>%
   dplyr::summarise_if(is.numeric,mean) %>%
   dplyr::mutate(rec_arousal = ifelse(segments.emotion.dimensions.activation>0, 1,0)) %>%
   dplyr::mutate(rec_valence = ifelse(segments.emotion.dimensions.valence>0, 1,0)) %>%
-  #dplyr::mutate_at(vars("emotion_valence_c","emotion_arousal_c","rec_valence","rec_arousal"), funs(as.factor(as.character(.)))) %>%
   as.data.frame()
 
 test$emotion_valence_c <- as.factor(as.character(test$emotion_valence_c))
@@ -29,11 +28,11 @@ test$emotion_arousal_c <- as.factor(as.character(test$emotion_arousal_c))
 test$rec_valence <- as.factor(as.character(test$rec_valence))
 test$rec_arousal <- as.factor(as.character(test$rec_arousal))
 
-str(test)
 valence_confusion_matrix <- caret::confusionMatrix(test$emotion_valence_c, test$rec_valence)
 arousal_confusion_matrix <- caret::confusionMatrix(test$emotion_arousal_c, test$rec_arousal)
 
-valence_confusion_matrix
+valence_confusion_matrix$overall["AccuracyLower"]
+
 
 
 ggplot(test, aes(emotion_valence, segments.emotion.dimensions.valence)) +
@@ -43,6 +42,10 @@ ggplot(test, aes(emotion_valence, segments.emotion.dimensions.valence)) +
 ggplot(test, aes(emotion_arousal, segments.emotion.dimensions.activation)) +
   geom_jitter() +
   stat_summary(fun.data = "mean_cl_boot", colour = "red", size = 1)
+
+##################################################################################
+
+
 
 ## data
 # 10 actors expressing:
@@ -94,3 +97,36 @@ ggplot(test, aes(emotion_arousal, segments.emotion.dimensions.activation)) +
 # anger* (ang)
 # relaxation
 # happiness
+
+test <- data.table::fread(here::here("./data","audeering_gemep.csv")) %>%
+  dplyr::filter(emotion %in% c("dis", "con","fea", "irr","anx", "int","joy", "pri","sad", "ang")) %>%
+  dplyr::select(name_audio, ppt, emotion,contains("emotion")) #%>%
+  dplyr::mutate(rec_dis = ifelse(segments.emotion.category_scores.disgust>0, 1,0)) %>%
+  dplyr::mutate(rec_con = ifelse(segments.emotion.category_scores.contentment>0, 1,0)) %>%
+  dplyr::mutate(rec_fea = ifelse(segments.emotion.category_scores.fear>0, 1,0)) %>%
+  dplyr::mutate(rec_irr = ifelse(segments.emotion.category_scores.irritation>0, 1,0)) %>%
+  dplyr::mutate(rec_anx = ifelse(segments.emotion.category_scores.worry>0, 1,0)) %>%
+  dplyr::mutate(rec_int = ifelse(segments.emotion.category_scores.interest>0, 1,0)) %>%
+  dplyr::mutate(rec_joy = ifelse(segments.emotion.category_scores.joy>0, 1,0)) %>%
+  dplyr::mutate(rec_pri = ifelse(segments.emotion.category_scores.pride>0, 1,0)) %>%
+  dplyr::mutate(rec_sad = ifelse(segments.emotion.category_scores.sadness>0, 1,0)) %>%
+  dplyr::mutate(rec_ang = ifelse(segments.emotion.category_scores.anger>0, 1,0))
+  
+test <- data.table::fread(here::here("./data","audeering_gemep.csv"))%>%
+  dplyr::filter(emotion %in% c("dis", "con","fea", "irr","anx", "int","joy", "pri","sad", "ang")) %>%
+  dplyr::mutate(emotion_c = case_when(
+    emotion == "dis" ~ "disgust",
+    emotion == "con" ~ "contentment",
+    emotion == "fea" ~ "fear",
+    emotion == "irr" ~ "irritation",
+    emotion == "anx" ~ "worry",
+    emotion == "int" ~ "interest",
+    emotion == "joy" ~ "joy",
+    emotion == "pri" ~ "pride",
+    emotion == "sad" ~ "sadness",
+    emotion == "ang" ~ "anger"
+  )) %>%
+  dplyr::mutate(match_emotion = ifelse(emotion_c == segments.emotion.category.name, 1, 0))
+
+scales::percent(sum(test$match_emotion)/nrow(test))
+
