90675ef77bd34676dc0d15bef53e91ae80938ed8
damien-dupre <damien.dupre.ac@gmail.com>
2019-07-03 17:24:51 +0100

    init

--- analysis/data_wrangling.R
+++ analysis/data_wrangling.R
@@ -1,14 +1,16 @@
-# Load package
+# libraries -----------------------------------------------------------------
 library(here)
 library(plyr)
 library(tidyverse)
 library(data.table)
-list_files <- list.files(here::here("./data/raw_data"), pattern = ".csv")
 
-raw_data <- plyr::ldply(list_files, function(file_name){
-  dat <- data.table::fread(here::here("./data/raw_data",file_name)) %>%
-    dplyr::mutate(file_ID = gsub(".mp3.csv","",file_name))%>%
-    dplyr::mutate(ppt_ID = substr(file_ID, 0, 2)) %>%
-    dplyr::mutate(ppt_emo = substr(file_ID, 3, 5))
-})
+# merge files ------------------------------------------------------------------
+audio_emotion_data <- "../../data/audio_emotion_data/csv_files" %>%
+  fs::dir_ls(regexp = "\\.csv$") %>%
+  purrr::map_dfr(readr::read_csv, .id = "source") %>%
+  dplyr::mutate(source = tools::file_path_sans_ext(base::basename(source))) %>%
+  dplyr::mutate(file_ID = gsub(".mp3", "", source)) %>%
+  dplyr::mutate(ppt_ID = substr(file_ID, 0, 2)) %>%
+  dplyr::mutate(ppt_emo = substr(file_ID, 3, 5))
 
+# readr::write_rds(audio_emotion_data, "audio_emotion_data.rds")
